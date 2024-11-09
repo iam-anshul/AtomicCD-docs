9844bc312fdf19f48170c637e58658ba0a93883f
Anshul Singh <anshu142000@gmail.com>
2024-11-09 19:26:35 +0530

    ADD url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: "img/logo.jpg",
 
   // Set the production url of your site here
-  url: "https://atomiccd.com",
+  url: "https://atomiccd.netlify.app/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
